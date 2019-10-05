2305bc64a02d53947f2dd79dc99a23cba3155e42
Styku0 <styku94@gmail.com>
2019-10-05 13:02:29 +0200

    Delete console logs

--- src/containers/details.tsx
+++ src/containers/details.tsx
@@ -3,9 +3,8 @@ import Details from '../pages/details';
 import { get } from 'lodash';
 
 const mapStateToProps = (state: any, props: any) => {
-    console.log({ state, props });
     const id = get(props, 'match.params.id');
-    console.log({ id });
+
     return ({
         data: get(state, `pokemon.list[${id}]`, {})
     })
