bbb6d18283364e8cc0566b272131e2152af1ad66
Styku0 <styku94@gmail.com>
2019-10-06 11:59:42 +0200

    Add a timeout to notification

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -2,6 +2,16 @@ import React, { FormEvent, useState, ChangeEvent } from 'react';
 import styled from 'styled-components';
 import { Redirect } from 'react-router';
 
+interface MessageBoxProps {
+    isError: boolean;
+    isProgress: boolean
+}
+const MessageBox = styled.div`
+    position: absolute;
+    width: 100%;
+    background: ${(props: MessageBoxProps) => props.isError ? 'tomato' : 'lightblue'};
+`;
+
 interface SearchProps {
     searchStart: any;
     status: string;
@@ -10,13 +20,18 @@ interface SearchProps {
 interface SearchState {
     query: string;
     previousStatus: string;
+    showMessage: boolean;
 }
 
 class Search extends React.Component<SearchProps, SearchState> {
     state = {
         previousStatus: this.props.status,
-        query: ''
+        query: '',
+        showMessage: false
     }
+
+    timeout: number = null;
+
 
     handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         this.setState({
@@ -37,20 +52,43 @@ class Search extends React.Component<SearchProps, SearchState> {
         const { status } = this.props;
         const { previousStatus } = this.state;
 
+
         if (status !== previousStatus) {
             this.setState({
-                previousStatus: status
+                previousStatus: status,
+                showMessage: true
             });
+
+            if (status === 'ERROR') {
+                this.timeout = setTimeout(() => this.setState({
+                    showMessage: false
+                }), 5000)
+            }
         }
     }
 
+    componentWillUnmount() {
+        clearTimeout(this.timeout);
+    }
+
     render() {
-        const { query, previousStatus } = this.state;
+        const { query, previousStatus, showMessage } = this.state;
         const { status } = this.props;
 
-        if (previousStatus === 'PROGRESS' && status === 'SUCCESS') {
+        const statusChanged = previousStatus !== status;
+        const isSuccess = status === 'SUCCESS';
+        const isProgress = status === 'PROGRESS';
+        const isError = status === 'ERROR';
+
+        if (statusChanged && isSuccess) {
             return (<Redirect to={`/search/${query}`} />);
         }
+
+        const messageBox = (
+            <MessageBox isError={isError} isProgress={isProgress} >
+                {isError ? 'Not found' : 'Searching...'}
+            </MessageBox>
+        )
 
         return (
             <form onSubmit={this.handleSubmit}>
@@ -60,8 +98,10 @@ class Search extends React.Component<SearchProps, SearchState> {
                     onChange={this.handleChange}
                     value={query}
                 />
+                {showMessage && messageBox}
                 <button type="submit">Search</button>
             </form>
+
         )
     }
 }
